7d476e01a7e8bc52b7a52db176f36fcb16848989
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-05-02 00:39:40 +0530

    switched to mainnet

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -5,7 +5,7 @@ import { log } from "@/utils/handlers";
 export let tonClient: TonClient = null as unknown as TonClient;
 
 export async function rpcConfig() {
-  const endpoint = await getHttpEndpoint({ network: "testnet" });
+  const endpoint = await getHttpEndpoint({ network: "mainnet" });
   tonClient = new TonClient({ endpoint });
   log("Ton client ready");
 }
