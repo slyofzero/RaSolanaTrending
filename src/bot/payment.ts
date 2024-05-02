8fc7a56a5edd2fd46edfa38c9adaac2f4c846803
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-05-02 12:45:16 +0530

    fixes

--- src/bot/payment.ts
+++ src/bot/payment.ts
@@ -331,8 +331,8 @@ Address Payment Received at - \`${hardCleanUpBotMessage(sentTo)}\``;
             dexSResonse,
           ]);
 
-          const terminalPool = terminalData.data.data.at(0);
-          const dexSPool = dexSData.data.pairs.at(0);
+          const terminalPool = terminalData.data.data?.at(0);
+          const dexSPool = dexSData.data.pairs?.at(0);
 
           const name =
             terminalPool?.attributes.name.split("/").at(0) ||
