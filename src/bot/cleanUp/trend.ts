28adef4e49ba0abff4569a92c4221b00874868f6
slyofzero <sly.of.zero@gmail.com>
2024-06-28 11:23:33 +0530

    Added token cleaup for low MC tokens

--- src/bot/cleanUp/trend.ts
+++ src/bot/cleanUp/trend.ts
@@ -1,6 +1,8 @@
 import { updateDocumentById } from "@/firebase";
-import { syncTrendingBuyBot } from "@/utils/api";
+import { PairsData } from "@/types";
+import { apiFetcher, syncTrendingBuyBot } from "@/utils/api";
 import { transactionValidTime } from "@/utils/constants";
+import { TOKEN_DATA_URL } from "@/utils/env";
 import { errorHandler, log } from "@/utils/handlers";
 import { getSecondsElapsed } from "@/utils/time";
 import { allToTrend, syncToTrend } from "@/vars/trending";
@@ -8,12 +10,20 @@ import { allToTrend, syncToTrend } from "@/vars/trending";
 export async function cleanUpPendingToTrend() {
   for (const trend of allToTrend) {
     try {
-      const { paidAt, expiresAt, id, status } = trend;
+      const { paidAt, expiresAt, id, status, token } = trend;
 
+      const pairData = await apiFetcher<PairsData>(
+        `${TOKEN_DATA_URL}/${token}`
+      );
+
+      const firstPair = pairData?.data.pairs.at(0);
       const secondsTillPaymentGeneration = getSecondsElapsed(paidAt.seconds);
       const currentTime = Math.floor(new Date().getTime() / 1e3);
+      const fdv = firstPair?.fdv || 0;
 
       if (
+        !firstPair ||
+        fdv < 2_500 ||
         (secondsTillPaymentGeneration > transactionValidTime &&
           status === "PENDING") ||
         (expiresAt &&
