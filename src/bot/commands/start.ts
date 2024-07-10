e1dcf0afbc19a7a3c17fc4ccc3167c680d7f91bc
slyofzero <sly.of.zero@gmail.com>
2024-07-10 21:07:01 +0530

    fixes

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -8,10 +8,12 @@ export async function startBot(ctx: CommandContext<Context>) {
   try {
     const text = `Welcome to ${BOT_USERNAME}\\!
     
-To trend a token - /trend
-To advertise - /advertise`;
+To trend a token \\- /trend
+To advertise \\- /advertise`;
 
     const { match } = ctx;
+
+    console.log(match);
 
     switch (match) {
       case "trend": {
@@ -23,6 +25,7 @@ To advertise - /advertise`;
         break;
       }
       default: {
+        console.log("test");
         return await ctx.reply(text, {
           // @ts-expect-error Type not found
           disable_web_page_preview: true,
